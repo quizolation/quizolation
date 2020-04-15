27492a7b531c6abbb88ddde6e1b028e155bb6931
Louise Spiers <louisespiers123@hotmail.com>
2020-04-15 21:55:32 +0100

    6 Removed some unnecessary imports

--- src/main/java/com/gavinfenton/quizolation/repository/QuizRepository.java
+++ src/main/java/com/gavinfenton/quizolation/repository/QuizRepository.java
@@ -1,10 +1,7 @@
 package com.gavinfenton.quizolation.repository;
 
 import com.gavinfenton.quizolation.entity.Quiz;
-import com.gavinfenton.quizolation.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
 
